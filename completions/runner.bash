c0a43ad24fc533be7705128c0127926d3d80ba60
Style Mistake <stylemistake@gmail.com>
2016-05-25 16:42:30 +0300

    Minor code tweaking

--- completions/runner.bash
+++ completions/runner.bash
@@ -1,5 +1,5 @@
 _runner() {
-  COMPREPLY=( $(compgen -W "$(runner -l)" -- "${COMP_WORDS[COMP_CWORD]}") )
+    COMPREPLY=( $(compgen -W "$(runner -l)" -- "${COMP_WORDS[COMP_CWORD]}") )
 }
 
 complete -F _runner runner
