b3b22354859631102516b4222a85713c9734b2f6
Style Mistake <stylemistake@gmail.com>
2016-07-13 00:29:50 +0300

    Add a task for tests

--- runnerfile.sh
+++ runnerfile.sh
@@ -1,6 +1,15 @@
-runner_default_task="shellcheck"
-source_files=(bin/* src/*)
+#!/usr/bin/env bash
+
+source_files=(bin/runner src/*.sh)
 
 task_shellcheck() {
     runner_run shellcheck --exclude=SC2155 "${source_files[@]}"
+}
+
+task_test() {
+    bash test/test.sh >/dev/null
+}
+
+task_default() {
+    runner_parallel shellcheck test
 }
