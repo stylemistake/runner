40c8020058bfad67977c5c76d03cb24474b82bed
Steven Kalt <kalt.steven@gmail.com>
2018-11-24 11:10:16 -0500

    Created setup.py to allow distribution as a python package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name="bash-task-runner",
+    version="0.8.0",
+    author="stylemistake",
+    author_email="stylemistake@gmail.com",
+    url="https://github.com/stylemistake/runner",
+    description="A Simple, lightweight task runner for Bash.",
+    license="LGPL-3.0",
+    scripts=["bin/runner"],
+    data_files=[
+        ('src', ['src/cli.sh', 'src/runner.sh']),
+        ('completion', ['completion/runner.bash']),
+    ]
+)
